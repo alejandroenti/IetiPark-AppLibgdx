e61f38a73c908884bc588088bb065e79fb176950
Alejandro López <116338818+alejandroenti@users.noreply.github.com>
2026-04-17 16:54:18 +0200

    Generating Main Menu Screen

---
+++ core/src/main/java/io/github/donquixote/ietipark/screens/MainMenu.java
@@ -0,0 +1,49 @@
+package io.github.donquixote.ietipark.screens;
+
+import com.badlogic.gdx.Screen;
+
+import io.github.donquixote.ietipark.DonQuixote;
+
+public class MainMenu  implements Screen {
+
+    final DonQuixote game;
+
+    public MainMenu(DonQuixote game) {
+        this.game = game;
+    }
+
+    @Override
+    public void show() {
+
+    }
+
+    @Override
+    public void render(float delta) {
+
+    }
+
+    @Override
+    public void resize(int width, int height) {
+
+    }
+
+    @Override
+    public void pause() {
+
+    }
+
+    @Override
+    public void resume() {
+
+    }
+
+    @Override
+    public void hide() {
+
+    }
+
+    @Override
+    public void dispose() {
+
+    }
+}
