53fb01af7e58739ffed9e15761ba6c06f74011ea
Alejandro López <116338818+alejandroenti@users.noreply.github.com>
2026-04-20 17:26:16 +0200

    Generating a method to initialize websocket conection

--- core/src/main/java/io/github/donquixote/ietipark/DonQuixote.java
+++ core/src/main/java/io/github/donquixote/ietipark/DonQuixote.java
@@ -5,7 +5,11 @@ import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 import com.badlogic.gdx.scenes.scene2d.ui.Skin;
 import com.badlogic.gdx.utils.viewport.ExtendViewport;
+import com.github.czyzby.websocket.WebSocket;
+import com.github.czyzby.websocket.WebSockets;
 
+import io.github.donquixote.ietipark.configuration.GameConfiguration;
+import io.github.donquixote.ietipark.infrastructure.WebSocketClient;
 import io.github.donquixote.ietipark.screens.MainMenu;
 
 /** {@link com.badlogic.gdx.ApplicationListener} implementation shared by all platforms. */
@@ -13,6 +17,8 @@ public class DonQuixote extends Game {
     public SpriteBatch batch;
     public ExtendViewport viewport;
     public Skin skin;
+    public WebSocket ws;
+    public GameConfiguration config;
 
     @Override
     public void create() {
@@ -32,5 +38,12 @@ public class DonQuixote extends Game {
     public void dispose() {
         batch.dispose();
         skin.dispose();
+    }
+
+    public void initializeWebSocketServer() {
+        ws = WebSockets.newSocket(WebSockets.toWebSocketUrl("10.0.2.2", 3000));
+        ws.setSendGracefully(false);
+        ws.addListener(new WebSocketClient(this));
+        ws.connect();
     }
 }
